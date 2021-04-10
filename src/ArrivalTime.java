e770f5fc96df9c8bb1b072e351952982b7390435
pozdnova <masha.pozdnova@gmail.com>
2021-04-10 15:19:05 +0300

    Add Arrival Time

--- src/ArrivalTime.java
+++ src/ArrivalTime.java
@@ -5,13 +5,15 @@ public class ArrivalTime {
     Calendar arrivalTime = new GregorianCalendar();
     private final int MONTH = 30;
     private final int HOURS = 24;
+    private final int MINUTES_AND_SECONDS = 60;
 
-    public ArrivalTime() {   //.set(тип времени, значение);
-        //число1 + Math.random()*число2
+    public ArrivalTime() {
+
         Calendar fistDate = new GregorianCalendar();
         arrivalTime.set(Calendar.DAY_OF_MONTH, (int) (fistDate.get(Calendar.DAY_OF_MONTH) + Math.random() * MONTH));
         arrivalTime.set(Calendar.HOUR_OF_DAY, (int) (Math.random() * (HOURS + 1)));
-        // (int)(0+Math.random()*24)
+        arrivalTime.set(Calendar.MINUTE, (int) (Math.random() * (MINUTES_AND_SECONDS + 1)));
+        arrivalTime.set(Calendar.SECOND, (int) (Math.random() * (MINUTES_AND_SECONDS + 1)));
     }
 
 
